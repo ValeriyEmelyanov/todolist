73d4fd97056030053feb1f15f4a14d180fce3423
Valery Emelyanov <emelva@rambler.ru>
2019-10-13 02:44:05 +0400

    Adding security

--- src/main/java/com/example/todolist/persist/repo/UserRepository.java
+++ src/main/java/com/example/todolist/persist/repo/UserRepository.java
@@ -4,6 +4,10 @@ import com.example.todolist.persist.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> getByUsername(String username);
+    boolean existsByUsername(String username);
 }
--- src/main/java/com/example/todolist/services/UserService.java
+++ src/main/java/com/example/todolist/services/UserService.java
@@ -4,6 +4,7 @@ import com.example.todolist.persist.entity.User;
 import com.example.todolist.persist.repo.UserRepository;
 import com.example.todolist.repr.UserRepr;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,15 +15,18 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void create(UserRepr userRepr) {
         User user = new User();
         user.setUsername(userRepr.getUsername());
-        user.setPassword(userRepr.getPassword());
+        user.setPassword(passwordEncoder.encode(userRepr.getPassword()));
         userRepository.save(user);
     }
 }
